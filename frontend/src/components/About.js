9fbb9c2aeba663e81cba7438009a980a47e14578
Nathan Meyer <nathan.n.meyer-1@ou.edu>
2023-09-12 19:32:29 -0500

    Created Baseline React Router, Still playing w it

---
+++ frontend/src/components/About.js
@@ -0,0 +1,7 @@
+import { Route, Routes } from "react-router-dom";
+
+const About = () => {
+  return <div>NOTAM INFORMATION PAGE</div>;
+};
+
+export default About;
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,7 @@
+import { Route, Routes } from "react-router-dom";
+
+const Home = () => {
+  return <div>PAGE WHERE PILOT INPUTS DEPARTURE AND ARRIVAL INFO</div>;
+};
+
+export default Home;
